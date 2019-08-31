a2eabbb1f1c13b1810f723b4baaac18b2cad8c5e
Thafarel <thafarel.camargo@zenvia.com>
2019-08-31 00:27:46 -0300

    GUI

--- navpatient/src/main/java/com/ufcspa/navpatient/controller/front/HomeController.java
+++ navpatient/src/main/java/com/ufcspa/navpatient/controller/front/HomeController.java
@@ -1,12 +1,13 @@
 package com.ufcspa.navpatient.controller.front;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/")
+@Controller
+@RequestMapping("/")
 public class HomeController {
 	
-	@GetMapping
+	@RequestMapping("/home")
 	public String novo() {
 		return "home";
 	}
